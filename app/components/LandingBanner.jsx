8cced17dc0a30232c760b79e5ac5cdc7dfd621a6
Senpai <clevercoder0307@gmail.com>
2025-10-02 12:39:12 +0530

    landingBanner updated as per changes

--- app/components/LandingBanner.jsx
+++ app/components/LandingBanner.jsx
@@ -1,31 +1,46 @@
-"use client"
+"use client";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import Image from "next/image";
 import React, { useRef } from "react";
 
-const LandingBanner = ({title}) => {
-  const landingBannerRef = useRef(null)
-  useGSAP(()=> (
+const LandingBanner = ({ title }) => {
+  const landingBannerRef = useRef(null);
+  useGSAP(() =>
     gsap.from(landingBannerRef.current, {
       opacity: 0,
-      delay: 0.4
+      delay: 0.4,
     })
-  ))
+  );
   return (
-    <div className="h-[60vh] w-screen">
+    <div className="h-[30vh] lg:h-[60vh] w-screen">
       <div className="relative h-full w-full">
         <div className="absolute h-full w-full">
           <div className="relative h-full w-full z-[-1]">
             <Image
-            className="object-cover"
-            src="/hero-bg.png"
-            alt="Hero-bg"
-            fill
-          />
+              className="object-cover"
+              src="/hero-bg.png"
+              alt="Hero-bg"
+              fill
+            />
           </div>
-          <div className="absolute bottom-0 left-0 px-6 py-5 md:px-10 md:py-10 lg:px-15 w-full">
-            <h1 ref={landingBannerRef} className="text-5xl lg:text-7xl xl:text-8xl font-poppins-semibold text-white text-center md:text-left">{title}</h1>
+          {/* <div className="relative lg:bottom-0 left-0 px-6 py-5 md:px-10 md:py-10 lg:px-15 w-full">
+            <div className="relative h-full w-full flex justify-center items-center">
+              <h1
+                ref={landingBannerRef}
+                className="text-5xl lg:text-7xl xl:text-8xl font-poppins-semibold text-white text-center md:text-left"
+              >
+                {title}
+              </h1>
+            </div>
+          </div> */}
+          <div className="absolute top-0 lg:bottom-0 h-full w-full flex items-center justify-center md:px-10 md:py-10 lg:px-15">
+            <h1
+                ref={landingBannerRef}
+                className="text-4xl lg:text-7xl xl:text-8xl font-poppins-semibold text-white text-center md:text-left"
+              >
+                {title}
+              </h1>
           </div>
         </div>
       </div>
